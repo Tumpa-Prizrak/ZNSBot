8870a3997fb09276cc021a073e29d4053a09bb83
Егор Брон <bataikinegor@yandex.ru>
2022-03-20 15:28:54 +0300

    Eval, logout and restort for disnake_test

--- disnake_test.py
+++ disnake_test.py
@@ -1,9 +1,20 @@
 import disnake, json
-from disnake.ext import commands
+from disnake.ext import commands, tasks
+import os, sys, asyncio, aiohttp, time, datetime, random, requests, platform, pyautogui, aeval
+
 
 json_data = json.load(open("settings.json", "r"))
 
 bot = commands.Bot(command_prefix=json_data["prefix"], case_insensitive=False, strip_after_prefix=True, intents=disnake.Intents.all())
+
+async def clean_code(content):
+    if content.startswith("```") and content.endswith("```"):
+        return "\n".join(content.split("\n")[1:])[:-3]
+    else:
+        return content
+minify_text = lambda txt: f'{str(txt)[:900]}...\n...и ещё {len(str(txt).replace(str(txt)[900:], ""))} символов...' if len(str(txt)) >= 1024 else str(txt)
+super_minify_text = lambda txt: f'{str(txt)[:132]}...\n...и ещё {len(str(txt).replace(str(txt)[132:], ""))} символов...' if len(str(txt)) >= 256 else str(txt)
+creators = [555638466365489172, 529302484901036043]
 
 @bot.event
 async def on_ready():
@@ -42,5 +53,74 @@ async def idea(interaction):
 
     await interaction.response.send_modal(MyModal())
 
+# мне пиздец лень переписывать под слеши
+@bot.command()
+async def logout(ctx, timerout: int = None):
+    if ctx.author.id in creators:
+        if timerout is None:
+            await ctx.send('Отключаюсь')
+            print('Отключение...')
+            await bot.close()
+            print('Отключено')
+        else:
+            await ctx.send(f'Отключение от Discord через {timerout} секунд')
+            await asyncio.sleep(timerout)
+            print('Отключение...')
+            await bot.close()
+            print('Отключено')
+    else:
+        await ctx.send("Нет! Тебе нельзя меня выключить!")
+@bot.command()
+async def restart(ctx, timerout: int = None):
+    if ctx.author.id in creators:
+        if timerout is None:
+            await ctx.send('Перезапуск!')
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        else:
+            await ctx.send(f'Отключение от Discord через {timerout} секунд')
+            await asyncio.sleep(timerout)
+            await ctx.send('Перезапуск!')
+            os.execl(sys.executable, sys.executable, *sys.argv)
+    else:
+        await ctx.send("Нет! Тебе нельзя меня перезагрузить!")
+@bot.command(aliases = ['eval', 'aeval', 'evaulate', 'выполнить', 'exec', 'execute'])
+async def __eval(ctx, *, arg):
+    if ctx.author.id not in bot.creators: return await ctx.send("Кыш!")
+    code = await clean_code(arg)
+    standart_args = {
+        "disnake": disnake,
+        "discord": disnake,
+        "commands": commands,
+        "bot": bot,
+        "tasks": tasks,
+        "ctx": ctx,
+        "asyncio": asyncio,
+        "aiohttp": aiohttp,
+        "os": os,
+        'sys': sys,
+        "time": time,
+        "datetime": datetime,
+        "random": random,
+        "requests": requests,
+        "pyautogui": pyautogui,
+        'platform': platform,
+        # 'logger': bot.logger
+    }
+    start = time.time()
+    try:
+        bot.logger.info(f'Invoked command "eval"')
+        r = await aeval.aeval(f"""{code}""", standart_args, {})#aioconsole.aexec(f"""{code}""", standart_args)
+        ended = time.time() - start
+        print(r)
+        if not code.startswith('#nooutput'):
+            await ctx.send(embed = disnake.Embed(title = "Успешно!", description = f"Выполнено за: {ended}", color = 0x99ff99).add_field(name = f'Входные данные:', value = f'`{minify_text(code)}`').add_field(name = f'Выходные данные:', value = f'`{minify_text(r)}`', inline=False))
+        bot.logger.success(f'Command "eval" executed in {ended}')
+    except Exception as e:
+        ended = time.time() - start
+        # await ctx.send(embed = discord.Embed(title = f"При выполнении возникла ошибка. Время: {ended}", description = f'Ошибка:\n```py\n{"".join(format_exception(e, e, e.__traceback__))}```', color = 0xff000))
+        if not code.startswith('#nooutput'):
+            await ctx.send(embed = disnake.Embed(title = f"При выполнении возникла ошибка.\nВремя: {ended}", description = f'Ошибка:\n```py\n{e}```', color = 0xff0000).add_field(name = f'Входные данные:', value = f'`{minify_text(code)}`', inline=False))
+        bot.logger.error(f'Command "eval" execution failed in {ended}:\n\t\tPython error: {e}')
+        raise e
 
 bot.run(json_data["token"])
