8c98d2338f2cde8f8ab168cefc264d3834f8297a
Егор Брон <bataikinegor@yandex.ru>
2022-03-20 15:44:28 +0300

    /idea remake (no modals)

--- disnake_test.py
+++ disnake_test.py
@@ -25,33 +25,40 @@ async def ping(interaction):
     await interaction.send(f"Pong in {round(bot.latency * 1000)} ms!")
 
 @bot.slash_command(name="idea", description="Send an idea for bot")
-async def idea(interaction):
-    class MyModal(disnake.ui.Modal):
-        def __init__(selfi) -> None:
-            super().__init__(title="Идея",
-                custom_id="idea_modal",
-                components=[
-                    disnake.ui.TextInput(
-                        label="Текст предложения",
-                        placeholder="Введите свою идею (макс. 1024 символа)",
-                        custom_id="idea_content",
-                        style=disnake.TextInputStyle.paragraph,
-                        min_length=5,
-                        max_length=1024,
-                    ),
-                ]
-            )
-        async def callback(selfi, interaction:disnake.ModalInteraction) -> None:
-            await interaction.response.send_message(embed=disnake.Embed(title="Спасибо за Вашу идею!", description=interaction.text_values["idea_content"]))
-            msg = await bot.get_channel(954016631430991942).send(embed=disnake.Embed(title=f"Идея от {interaction.author} ({interaction.author.id})", description=interaction.text_values["idea_content"]))
-            await msg.create_thread(name = "Обсуждение")
-            await msg.add_reaction("👍")
-            await msg.add_reaction("👎")
+async def idea(interaction, idea: str = commands.Param(
+    name = 'Idea', description ="Text of your idea"
+)):
+    # class MyModal(disnake.ui.Modal):
+    #     def __init__(selfi) -> None:
+    #         super().__init__(title="Идея",
+    #             custom_id="idea_modal",
+    #             components=[
+    #                 disnake.ui.TextInput(
+    #                     label="Текст предложения",
+    #                     placeholder="Введите свою идею (макс. 1024 символа)",
+    #                     custom_id="idea_content",
+    #                     style=disnake.TextInputStyle.paragraph,
+    #                     min_length=5,
+    #                     max_length=1024,
+    #                 ),
+    #             ]
+    #         )
+    #     async def callback(selfi, interaction:disnake.ModalInteraction) -> None:
+    #         await interaction.response.send_message(embed=disnake.Embed(title="Спасибо за Вашу идею!", description=interaction.text_values["idea_content"]))
+    #         msg = await bot.get_channel(954016631430991942).send(embed=disnake.Embed(title=f"Идея от {interaction.author} ({interaction.author.id})", description=interaction.text_values["idea_content"]))
+    #         await msg.create_thread(name = "Обсуждение")
+    #         await msg.add_reaction("👍")
+    #         await msg.add_reaction("👎")
 
-        async def on_error(selfi, error, inter) -> None:
-            await inter.response.send_message("Упс... Что-то пошло не так...", ephemeral=True)
+    #     async def on_error(selfi, error, inter) -> None:
+    #         await inter.response.send_message("Упс... Что-то пошло не так...", ephemeral=True)
 
-    await interaction.response.send_modal(MyModal())
+    # await interaction.response.send_modal(MyModal())
+    await interaction.response.send_message(embed=disnake.Embed(title="Спасибо за Вашу идею!", description=idea))
+    msg = await bot.get_channel(954016631430991942).send(embed=disnake.Embed(title=f"Идея от {interaction.author} ({interaction.author.id})", description=idea))
+    await msg.create_thread(name = "Обсуждение")
+    await msg.add_reaction("👍")
+    await msg.add_reaction("👎")
 
 # мне пиздец лень переписывать под слеши
 @bot.command()
