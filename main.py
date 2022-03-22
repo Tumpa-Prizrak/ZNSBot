9531d00fed36d26f6ac6f625bd550b5ff23296d7
Егор Брон <bataikinegor@yandex.ru>
2022-03-22 12:53:32 +0300

    Idea!

--- main.py
+++ main.py
@@ -50,17 +50,22 @@ async def nekos_(interaction: nextcord.Interaction, category: str = nextcord.Sla
 @bot.slash_command(name = "rule34", description = "Получает картинку из Rule34 (только в NSFW-каналах)")
 async def rule34(interaction: nextcord.Interaction, tag: str = nextcord.SlashOption(name='tag', description = "Тег, по которому нужно найти картинку", required = True)):
     if not interaction.channel.is_nsfw(): return await interaction.send("Вы не можете воспользоваться командой вне NSFW-канала")
-    apibase = "https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1&tags={}&json=1"
+    apibase = "https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=20&tags={}&json=1"
     # так как аиохттп умерла, сидим на реквестс
     # async with aiohttp.ClientSession() as session:
     #     async with session.get(apibase.format(tag)) as response:
     #         if (await response.text()) == '':
     #             return await interaction.send('По данному тегу ничего не нашлось :(')
-    #         r = (await response.json())[0]
+    #         r = random.choice((await response.json()))
     response = requests.get(apibase.format(tag))
     if response.text == '': return await interaction.send('По данному тегу ничего не нашлось :(')
-    r = response.json()[0] # TODO: ujson.loads
+    r = random.choice(response.json()) # TODO: ujson.loads
     await interaction.send(embed = nextcord.Embed().set_footer(text=f"Теги: {minify_text(r.get('tags', 'нет'))}").set_image(url = r.get('file_url')))
+
+# Модалки надо бы...
+# @bot.user_command(name='Пожаловаться', description = 'Пожаловаться на пользователя')
+# async def complaint_user_command(interaction: nextcord.Interaction):
+#     pass
 
 # @bot.command(aliases = ['eval', 'aeval', 'evaulate', 'выполнить', 'exec', 'execute'])
 # async def __eval(ctx, *, arg):
